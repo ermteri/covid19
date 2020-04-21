89d7be0a359f1134cea3d71cc3fb70237791ab6f
ermteri <ermteri@gmail.com>
2020-04-21 21:48:44 +0200

    Initial commit

---
+++ covid_stats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import csv
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+import sys
+
+ECDC_URL = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv'
+
+
+class CovidStats:
+    def __init__(self):
+        self.csv_url = ECDC_URL
+        # fields = ['dateRep', 'day', 'month', 'year', 'cases', 'deaths', 'countriesAndTerritories', 'geoId',
+        #          'countryterritoryCode', 'popData2018', 'continentExp']
+
+    def moving_average(self, a, n=3):
+        ret = np.cumsum(a, dtype=int)
+        ret[n:] = ret[n:] - ret[:-n]
+        return ret[n - 1:] / n
+
+    def get_csv(self):
+        req = requests.get(self.csv_url)
+        return csv.DictReader(req.text.splitlines())
+
+    def print_graph(self, csv_reader):
+        fig = plt.subplots()
+        x = list()
+        y = list()
+        for row in csv_reader:
+            date_time_obj = datetime.datetime.strptime(row['dateRep'], '%d/%m/%Y')
+            x.append(date_time_obj)
+            y.append(int(row['deaths']))
+        plt.xticks(rotation=90)
+        plt.grid()
+        plt.plot(x[:len(x)-6], self.moving_average(y, 7))
+        plt.show()
+        pass
+
+
+def run(args):
+    cs = CovidStats()
+    sweden = (row for row in cs.get_csv() if row['countriesAndTerritories'] == 'Sweden')
+    cs.print_graph(sweden)
+
+
+if __name__ == '__main__':
+    run(sys.argv)
+
